9f4ec376b5ee0ce23d33bfca012bf319efc4f3d2
VladZernov <vlad.zernov@yahoo.com>
2016-12-17 23:58:47 +0200

    added methods for retrieving cars and serching nearest showroom

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -30,14 +30,47 @@ class DefaultController extends Controller
      * @Route("/showroom", name="showroom")
      * @Method({"GET"})
      */
-    public function getNearestShowroom() {}
+    public function getNearestShowroom(Request $request) {
+
+        $x = $request->get('x');
+        $y = $request->get('y');
+
+        $em = $this->getDoctrine()->getManager();
+
+        $nearestShowroom = $em
+            ->createQueryBuilder()
+            ->select('s.id, s.title, s.address, s.phone, sqrt(pow(s.longtitude - :x, 2) + pow(s.latitude - :y, 2)) as distance')
+            ->from('AppBundle:Showroom','s')
+            ->orderBy('distance','asc')
+            ->setMaxResults(1)
+            ->setParameter('x', $x)
+            ->setParameter('y', $y)
+            ->getQuery()
+            ->getSingleResult();
+
+        return $this->json($nearestShowroom);
+    }
     /**
      * @Route("/models/{showroom}", name="models", requirements={
      *     "showroom": "\d+"
      * })
      * @Method({"GET"})
      */
-    public function getModels() {}
+    public function getModels($showroom) {
+        $em = $this->getDoctrine()->getManager();
+
+        $cars = $em
+            ->createQueryBuilder()
+            ->select('c.model, c.price')
+            ->from('AppBundle:Car','c','c.id')
+            ->innerJoin('c.showrooms', 's')            
+            ->where('s.id = :showroom_id')
+            ->setParameter('showroom_id', $showroom)
+            ->getQuery()
+            ->getResult();
+
+        return $this->json($cars);        
+    }
     public function markModel () {}
     public function unmarkModel() {}
     public function getMarkedModels() {}
